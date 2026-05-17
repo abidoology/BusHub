53cc0ecb352fa817a1e073f02d9aa3c95d3593fd
abidoology <habid3328@gmail.com>
2026-05-17 22:52:25 +0600

    add station and fare fields to BusModel

--- lib/models/bus_models.dart
+++ lib/models/bus_models.dart
@@ -5,9 +5,17 @@ class BusModel {
   final String busId;
   final String busName;
 
+  // Ordered list of stations
+  final List<String> stations;
+
+  // Fare from source to each station
+  final Map<String, double> faresFromSource;
+
   BusModel({
     this.documentId,
     required this.busId,
     required this.busName,
+    required this.stations,
+    required this.faresFromSource,
   });
 }
